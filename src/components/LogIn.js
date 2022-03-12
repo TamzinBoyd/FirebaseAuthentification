dfddac48e82f33f17b22b1aa741ce44b83c308f8
TamzinBoyd <tamzin_boyd@hotmail.co.uk>
2022-03-12 12:23:34 +0000

    authorisation added

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,15 +1,33 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/UserProvider";
-import { app } from "../firebase";
+import { firestore } from "../firebase";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LogIn = () => {
+  // import UserContext object
   const userContext = useContext(UserContext);
 
   const handleSubmit = (e) => {
-    // do this to stop it refreshing the page on submit
-    e.prevent.default();
+    // stop it refreshing the page on submit
+    e.preventDefault();
+
+    // get value from inpout boxes
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    // inbuilt function, getAuth funct passes in the in firebase.js file then the values from form as saved in variables above
+    // returns a promise
+    signInWithEmailAndPassword(getAuth(firestore), email, password).then(
+      ((response) => {
+        //   it returns a user credential, given as a user then you can access their email (this isn't related to above variable). setUser is the function to update state, so we're changing the value of user (state) to the response object.user.email - so now the user's email shows on the screen rather than their name
+        userContext.setUser(response.user.email);
+        alert("Welcome back!");
+      }).catch((e) => {
+        alert("Sorry, something went wrong");
+      })
+    );
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
--- src/context/UserProvider.js
+++ src/context/UserProvider.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from "react";
 import { Home } from "../components/Home";
 import Nav from "../components/Nav";
+import LogIn from "../components/LogIn";
 
 // this is the way the data is passed around through the app, info is held in userContext.Initial value set to empty object
 export const UserContext = createContext({});
@@ -19,6 +20,7 @@ const Provider = () => {
     <UserContext.Provider value={user}>
       <Nav />
       <Home />
+      <LogIn />
     </UserContext.Provider>
   );
 };
